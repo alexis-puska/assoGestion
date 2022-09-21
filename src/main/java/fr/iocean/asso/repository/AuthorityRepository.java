c18a6837a9442d5819a849309e3c38e2e726254f
Alexis <a.puska@ile-noire.com>
2022-09-21 17:58:37 +0200

    Initial version of assoGestion generated by generator-jhipster@7.7.0

---
+++ src/main/java/fr/iocean/asso/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package fr.iocean.asso.repository;
+
+import fr.iocean.asso.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/fr/iocean/asso/IntegrationTest.java
@@ -0,0 +1,17 @@
+package fr.iocean.asso;
+
+import fr.iocean.asso.AssoGestionApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = AssoGestionApp.class)
+public @interface IntegrationTest {
+}
